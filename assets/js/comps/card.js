05d8e7bd9da187e51a778f140c4109e03900ecb7
Ahmed Hussein <50411535+huss0249@users.noreply.github.com>
2024-08-08 15:20:05 -0400

    start

--- assets/js/comps/card.js
+++ assets/js/comps/card.js
@@ -1,1 +1,119 @@
 const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    @import url("./assets/css/lib/bootstrap.min.css");
+    @import url("./assets/css/lib/icons/bootstrap-icons.css");
+    @import url("./app2023.css");
+
+    :root {
+      display: block;
+    }
+
+    .note {
+      // border-radius: 1rem;
+    }
+    .shadow {
+      box-shadow: 0px 0px 10px rgba(0, 0, 0, .25) !important;
+    }
+  </style>
+
+  <div class="root card border-light border-2 mb-3 shadow note">
+    <div class="card-header">
+      <i class="bi bi-bookmark h3"></i> <slot name="title" class="h4 fw-light">Note</slot>
+    </div>
+    <div class="card-body text-dark bg-body-white">
+      <slot name="cardbody">Default text content</slot>
+    </div>
+  </div>
+`;
+
+class customCard extends HTMLElement {
+  constructor() {
+    super();
+    this.root = this.attachShadow({ mode: 'closed' });
+    let clone = template.content.cloneNode(true);
+    this.root.append(clone);
+    // console.log('comp Note created')
+  }
+
+  //define the allowed attributes
+  static get observedAttributes() {
+    return ['cat'];
+  }
+
+  //handle values and changes to the attributes
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    if (attrName.toLowerCase() === 'cat') {
+      let colorVal = ''
+      let iconVal = ''
+
+      if (newVal === 'note') {
+        colorVal = 'warning'
+        iconVal = 'sticky'
+      } else if (newVal === 'res') {
+        colorVal = 'info'
+        iconVal = 'book'
+      } else if (newVal === 'tip') {
+        colorVal = 'success'
+        iconVal = 'lightbulb'
+      } else if (newVal === 'info') {
+        colorVal = 'primary'
+        iconVal = 'info-circle'
+      } else {
+        colorVal = 'dark'
+        iconVal = 'info-lg'
+      }
+
+      let icon = this.root.querySelector('i')
+      // console.log(icon)
+      icon.classList.remove('bi-bookmark');
+      icon.classList.add(`bi-${iconVal}`);
+
+      // this.classList.add(`bg-${colorVal}`);
+      // this.classList.add(`border-${colorVal}`);
+      this.classList.add(`bg-${colorVal}`, `border-${colorVal}`);
+
+      let header = this.root.querySelector('.card-header')
+      // console.log(header)
+      // header.classList.add(`bg-${colorVal}-subtle`);
+      // header.classList.add(`text-${colorVal}-emphasis`);
+      header.classList.add(`bg-${colorVal}-subtle`, `text-${colorVal}-emphasis`);
+
+      icon.classList.add(`text-${colorVal}`);
+    }
+
+
+    if (attrName.toLowerCase() === 'color') {
+      // this.classList.add(`bg-${newVal}`);
+      // this.classList.add(`border-${newVal}`);
+      this.classList.add(`bg-${newVal}`, `border-${newVal}`);
+
+      let header = this.root.querySelector('.card-header')
+      console.log(header)
+      // header.classList.add(`bg-${colorVal}-subtle`);
+      // header.classList.add(`text-${colorVal}-emphasis`);
+      header ? header.classList.add(`bg-${colorVal}-subtle`, `text-${colorVal}-emphasis`) : "";
+
+      let icon = this.root.querySelector('i')
+      icon.classList.add(`text-${newVal}`);
+    }
+
+    if (attrName.toLowerCase() === 'icon') {
+      let icon = this.root.querySelector('i')
+      // console.log(icon)
+      icon.classList.remove('bi-info-circle');
+      icon.classList.add(`bi-${newVal}`);
+    }
+  }
+
+
+  //sync attributes with properties as you want
+  get cat() {
+    return this.getAttribute('cat');
+  }
+  set cat(value) {
+    this.setAttribute('cat', value);
+  }
+
+}
+customElements.define('tdd-card', customCard);
---
+++ assets/js/comps/excustomAccordion.js
@@ -0,0 +1,114 @@
+const template = document.createElement("template");
+template.innerHTML = `
+<link href="./assets/css/lib/animate.min.css" rel="stylesheet" crossorigin="anonymous">
+<link href="./assets/css/lib/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+<link href="./assets/css/lib/icons/bootstrap-icons.css" rel="stylesheet" crossorigin="anonymous">
+<!-- <link href="./app2023.css" rel="stylesheet" crossorigin="anonymous">   -->
+<!--
+-->
+<script src="./assets/js/lib/bootstrap.bundle.min.js" type="module" crossorigin="anonymous" defer></script>
+<style>
+
+</style>
+
+<div>
+  <h1></h1>
+  <div>
+    <slot name = 'content'></slot>
+  </div>
+</div>
+`;
+
+class ExcustomAccordion extends HTMLElement {
+  constructor() {
+    super();
+    this.root = this.attachShadow({ mode: "open" });
+    let clone = template.content.cloneNode(true);
+    this.root.append(clone);
+  }
+
+  static get observedAttributes() {
+    return ["cat", "color"];
+  }
+  connectedCallback() {
+    this.wc_title();
+    // this.wc_content();
+
+    this.makeAccordions();
+
+    // return;
+  }
+
+  wc_title() {
+    let $title = this.root.querySelector("h1");
+    $title.textContent === "" ? $title.remove() : ''
+    // $title.textContent = this.id;
+  }
+
+  wc_content() {
+    // this.$content = this.root.querySelector("slot").assignedElements()[0];
+    this.makeAccordions();
+  }
+
+  makeAccordions() {
+    this.$content = this.root.querySelector("div slot").assignedElements()[0];
+
+    let targetSelector = "div";
+    let boxes = this.$content.children;
+
+    Array.from(boxes).forEach(($acc, idx) => {
+      let parentName = $acc.parentElement.parentElement.id;
+      $acc.id = `${parentName}${idx}`;
+
+      let $accs = $acc.querySelectorAll("div");
+
+      let df = new DocumentFragment();
+
+      // LOOP
+      $accs.forEach((AccEl, iddx) => {
+        let accItem = document.createElement("div");
+        accItem.classList.add("accordion-item");
+
+        let accBtn = document.createElement("button");
+        accBtn.type = "button";
+        accBtn.classList.add("accordion-button", "collapsed");
+        accBtn.setAttribute("data-bs-toggle", "collapse");
+        accBtn.setAttribute(
+          "data-bs-target",
+          `#${parentName}${idx}-collapse_${iddx}`
+        );
+        accBtn.setAttribute("aria-expanded", "false");
+        accBtn.setAttribute(
+          "aria-controls",
+          `${parentName}${idx}-collapse_${iddx}`
+        );
+
+        let accCollapse = document.createElement("div");
+        accCollapse.id = `${parentName}${idx}-collapse_${iddx}`;
+        accCollapse.classList.add("accordion-collapse", "collapse", "d-shadow");
+        accCollapse.setAttribute("data-bs-parent", `#${parentName}${idx}`);
+
+        let accHeader = AccEl.querySelector("h2");
+        // console.log("accHeader is", accHeader)
+        if(accHeader) {
+          accHeader.classList.add("accordion-header")
+          accBtn.textContent = accHeader.textContent
+          accHeader.innerHTML = "";
+          accHeader.appendChild(accBtn);
+          accItem.appendChild(accHeader);
+        }
+
+        let accBody = AccEl;
+        accBody.classList.add("accordion-body");
+        accCollapse.appendChild(accBody);
+        accItem.appendChild(accCollapse);
+        df.appendChild(accItem);
+        $acc.appendChild(df);
+        $acc.classList.add("accordion", "accordion-flush");
+      });
+
+      return $acc;
+    });
+  }
+}
+customElements.define("tdd-excustomaccordion", ExcustomAccordion);
