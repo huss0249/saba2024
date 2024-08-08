b1172492ece20bc7a72d5a8e7c77999953915e93
Ahmed Hussein <50411535+huss0249@users.noreply.github.com>
2024-08-08 15:11:03 -0400

    Add files via upload

---
+++ importer.js
@@ -0,0 +1,34 @@
+// import "./assets/js/comps/player.js";
+
+// import MyHiFiRectangle from "./assets/js/comps/level2.js"
+
+
+
+import "./assets/js/comps/footer.js";
+import "./assets/js/comps/page.js";
+
+import "./assets/js/comps/hero.js";
+import "./assets/js/comps/image.js";
+// import "./assets/js/comps/customimg.js";
+// import "./assets/js/comps/jumbo.js";
+import "./assets/js/comps/card.js";
+import "./assets/js/comps/instructions.js";
+import "./assets/js/comps/note.js";
+import "./assets/js/comps/table.js";
+import "./assets/js/comps/expanable.js";
+import "./assets/js/comps/exAccordion.js";
+import "./assets/js/comps/excustomAccordion.js";
+
+import "./assets/js/comps/customInteraction.js";
+
+/*
+import "./assets/js/comps/interactionA.js";
+import "./assets/js/comps/interactionB.js";
+import "./assets/js/comps/interactionC.js";
+import "./assets/js/comps/interactionD.js";
+*/
+
+import "./assets/js/comps/exTab.js";
+// import "./assets/js/comps/exPill.js";
+// import "./assets/js/comps/exHorizontal.js";
+// import "./imgs.js";
