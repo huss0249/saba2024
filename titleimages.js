85ed00ea1f708ed0c284e11a11e567b4fdfb6968
Ahmed Hussein <50411535+huss0249@users.noreply.github.com>
2024-08-08 15:10:34 -0400

    Add files via upload

---
+++ titleimages.js
@@ -0,0 +1,42 @@
+window.titleImages = [
+    {
+        'name': "rpp1m1img1",
+        'image': "RPP_C1_M1_01",
+        'alt': "A torpedo being loaded on to ship by military members without respiratory protection."
+    },
+    {
+        'name': "rpp1m1img2",
+        'image': "RPP_C1_M1_02",
+        'alt': "Two military members with no respiratory protection standing next to torpedoes on a cart."
+    },
+    {
+        'name': "rpp1m1img3",
+        'image': "RPP_C1_M1_03",
+        'alt': "Two military members with no respiratory protection, sitting beside torpedoes and showing signs of medical discomfort."
+    },
+    {
+        'name': "rpp1m1img4",
+        'image': "RPP_C1_M1_04",
+        'alt': "Torpedoes on a cart in a cordoned off area."
+    },
+    {
+        'name': "rpp1m3img1",
+        'image': "RPP_C1_M3_01",
+        'alt': "Cross section of the human respiratory system."
+    },
+    {
+        'name': "rpp1m3img2",
+        'image': "RPP_C1_M3_02",
+        'alt': "A mechanic wearing a reusable elastomeric respirator while fixing a car."
+    },
+    {
+        'name': "rpp1m3img3",
+        'image': "RPP_C1_M3_03",
+        'alt': "A firefighter wearing a self-contained breathing apparatus while conducting a vehicle extraction."
+    },
+    {
+        'name': "rpp1m3img4",
+        'image': "RPP_C1_M3_04",
+        'alt': "A painter using a supplied-air respirator while doing paint work in a paint booth."
+    }
+]
